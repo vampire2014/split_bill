3357799f1d3414fdc71ee30718ee9a30a7e90c4b
vampire2014 <zx1989@gwmail.gwu.edu>
2015-02-26 21:53:40 -0500

    Updated

--- app/src/main/java/com/eugeneashley/splitbill/authentication/LoginActivity.java
+++ app/src/main/java/com/eugeneashley/splitbill/authentication/LoginActivity.java
@@ -60,13 +60,24 @@ public class LoginActivity extends Activity{
             public void onClick(View view) {
                 final Dialog dialog = new Dialog(context);
                 dialog.setContentView(R.layout.login_activity);
+                TextView forgotpassword = (TextView) dialog.findViewById(R.id.et_forgotpassword);
+                forgotpassword.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+//                        username = ((AutoCompleteTextView) v.findViewById(R.id.et_email)).getText().toString();
+//                        password = ((EditText) v.findViewById(R.id.et_password)).getText().toString();
+                        Intent intent = new Intent(LoginActivity.this, ResetActivity.class);
+                        startActivity(intent);
+                    }
+                });
                 Button dialogButton = (Button) dialog.findViewById(R.id.b_login);
+
                 // if button is clicked, close the custom dialog
                 dialogButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        username = ((AutoCompleteTextView) v.findViewById(R.id.et_email)).getText().toString();
-                        password = ((EditText) v.findViewById(R.id.et_password)).getText().toString();
+//                        username = ((AutoCompleteTextView) v.findViewById(R.id.et_email)).getText().toString();
+//                        password = ((EditText) v.findViewById(R.id.et_password)).getText().toString();
                         dialog.dismiss();
                     }
                 });
@@ -81,53 +92,6 @@ public class LoginActivity extends Activity{
                 openfbLoginViewer();
             }
         });
-    }
-    private void openSignupViewer() {
-        LayoutInflater inflater = getLayoutInflater();
-        View dialoglayout = inflater.inflate(R.layout.signup_activity,null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this,AlertDialog.THEME_HOLO_LIGHT);
-
-        builder.setTitle(getString(R.string.signup));
-        builder.setView(dialoglayout);
-        builder.setCancelable(false).setPositiveButton(getString(R.string.label_sign_up),new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int id) {
-                Dialog f = (Dialog) dialogInterface;
-                username = ((AutoCompleteTextView) f.findViewById(R.id.et_email)).getText().toString();
-                password = ((EditText) f.findViewById(R.id.et_password)).getText().toString();
-                //success
-                //failure
-            }
-        });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
-
-    private void openLoginViewer() {
-        LayoutInflater inflater = getLayoutInflater();
-        View dialoglayout = inflater.inflate(R.layout.login_activity,null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this,AlertDialog.THEME_HOLO_LIGHT);
-        builder.setView(dialoglayout);
-        builder.setCancelable(false).setPositiveButton(getString(R.string.label_sign_up), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int id) {
-                Dialog f = (Dialog) dialogInterface;
-                username = ((AutoCompleteTextView) f.findViewById(R.id.et_email)).getText().toString();
-                password = ((EditText) f.findViewById(R.id.et_password)).getText().toString();
-                //success
-                //failure
-            }
-        });
-        forgotpassword = (TextView) this.findViewById(R.id.et_forgotpassword);
-        forgotpassword.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, ForgetActivity.class);
-                startActivity(intent);
-            }
-        });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
     }
 
     private void openfbLoginViewer() {
